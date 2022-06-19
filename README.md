da0f6913907cf027c10aac0fc2c862d4cdfe02b9
Eric <89803837+ericf1@users.noreply.github.com>
2022-06-18 23:01:30 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,119 @@
+# Announcements
+
+The bot will go down for Instagram at 12AM 6/17 EST
+
+# FTWBot
+
+<!-- <img src="https://user-images.githubusercontent.com/89803837/160317016-a68164d4-a6fa-4fc4-8957-db3093f65c73.png" alt="Basket with social medias inside" width=300rem height=300rem> -->
+<img src="https://github.com/ericf1/ftwbot/blob/main/pictures/ftw%20logo.png" width=300rem height=300rem>
+
+A Discord bot that continuously updates and posts Instagram and Twitter posts from designated accounts. 
+
+We are currently supporting over 194 servers!
+
+Visit our [Top.gg](https://top.gg/bot/952690377104719964) page!
+
+Add FTWBot To Your Server!
+---
+Click on the link and authenticate:
+[Link](https://discord.com/api/oauth2/authorize?client_id=952690377104719964&permissions=517543905344&scope=bot)
+
+If the first link is full:
+[Second Link](https://discord.com/api/oauth2/authorize?client_id=978880834440429578&permissions=277025474624&scope=bot)
+
+Features
+---
+- Easy to run
+- Constant uptime
+- Supports both Instagram and Twitter
+
+Commands
+---
+FTWBot's prefix is ``s!``, add it to the start of any of this bot's command. All of the commands are case-sensitive.
+
+| Command | Arguments | Description | Example |
+|---------|-----------|-------------|---------|
+| setChannel | ``{#channel}`` | Sets the channel the bot posts in | ``s!setChannel #general``|
+| list | None | Displays a list of all of the social media accounts being tracked | ``s!list``|
+| add |``{social-media-platform} {username}`` | Adds a social media account to the list of accounts being tracked | ``s!add twitter lsxyz9`` | 
+| remove | ``{social-media-platform} {username}`` | Removes a social media account from the list of accounts being tracked | ``s!remove twitter Cloud9``|
+| ping | None | Pong | ``s!ping`` |
+
+Requirements!
+---
+
+- **For posts to send you must have a channel set first with s!setChannel**
+
+- **You also must ensure that the bot has permission to write in that channel or it will not be able to post!**
+
+Future
+---
+- Implementing Reddit and YouTube APIs
+- Creating custom web links for Discord webhooks
+- Implementing Redis Database
+
+
+Suggestions are appreciated and can be posted to the [Discussions](https://github.com/ericf1/ftwbot/discussions) page.
+
+How To Set Up Your Own Bot From Your Personal Machine
+---
+>First, fetch our code base through git clone:
+```
+git clone https://github.com/ericf1/ftwbot.git
+```
+or through downloading the source code that is avaliable in the [versions](https://github.com/ericf1/ftwbot/releases)
+
+>Install the necessary libraries by running:
+```
+pip3 install -r requirements.txt
+```
+
+>Change directories to the python code files with:
+```
+cd src
+```
+
+>Create a .env file containing the following inside of the src folder with vim or your favorite text editor:
+```
+vim .env
+```
+
+>You will need a personal Discord Token, Twitter API Key, and Twitter Access Token. Your .env file should be formatted like this:
+```
+TWITTER_API_KEY=
+TWITTER_API_SECRET_KEY=
+TWITTER_ACCESS_TOKEN=
+TWITTER_ACCESS_TOKEN_SECRET=
+DISCORD_TOKEN=
+```
+You can find these keys from the [Twitter Developer Portal](https://developer.twitter.com/en/portal/petition/essential/basic-info) and [Discord Developer Portal](https://discord.com/developers/docs/intro)
+
+>In vim, to save and exit, execute:
+```
+:wq
+```
+
+>Start the bot with:
+```
+python3 bot.py
+```
+
+Our Python code is currently set to check for new posts every ten minutes (this can be changed in under the parameter of the mainLoop).
+
+In order to constantly maintain the bot, we recommend running the Python script through a Cloud Hosting Service such as [Amazon Web Service](https://aws.amazon.com/). We currently run an instance of ubuntu through AWS. 
+
+Questions and Inquiries
+---
+[ericfang1935@gmail.com](mailto:ericfang1935@gmail.com)
+
+Technologies Used
+---
+Python, Discord.py, Tweepy API, TinyDB, Redis, Google API, Reddit API, Amazon Web Server
+
+Made For
+---
+[The Dive Studios's Timmy Bot](https://www.divestudios.io/)
+
+Developers
+---
+Eric Fang, Wilson Wuchen, Edison Tran, Trinnity Ye (Marketing), Ella Tang (Marketing)
---
+++ src/bot.py
@@ -0,0 +1,241 @@
+from webbrowser import get
+from dotenv import load_dotenv
+import os
+from turtle import update
+from tinydb import TinyDB
+from instagramAPI import getLatestInstagramPosts, checkInstagramUser
+from twitterAPI import getLatestTwitterPosts, checkTwitterUser
+from discord.ext import commands, tasks
+import discord
+
+import logging
+import time
+from datetime import datetime, timezone
+load_dotenv()
+
+socialsData = {
+    "instagram": {
+        "icon": "https://www.instagram.com/static/images/ico/favicon-192.png/68d99ba29cc8.png",
+        "color": 13453419
+    },
+    "twitter": {
+        "icon": "https://abs.twimg.com/responsive-web/client-web/icon-ios.b1fc7275.png",
+        "color": 44270
+    }
+}
+
+# Setup database
+db = TinyDB('database.json')
+
+
+def doc(server_id):
+    table = db.table(str(server_id))
+
+    if not table.get(doc_id=1):
+        table.insert({"socials": {}})
+
+    for socialMedia in socialsData.keys():
+        if socialMedia not in table.get(doc_id=1)["socials"].keys():
+            socialMediaObject = table.get(doc_id=1)["socials"]
+            socialMediaObject[socialMedia] = []
+
+            table.update({"socials": socialMediaObject})
+
+    return table.get(doc_id=1)
+
+
+def updateDoc(server_id, obj):
+    table = db.table(str(server_id))
+
+    if not table.get(doc_id=1):
+        table.insert({"socials": {}})
+
+    for socialMedia in socialsData.keys():
+        if socialMedia not in table.get(doc_id=1)["socials"].keys():
+            socialMediaObject = table.get(doc_id=1)["socials"]
+            socialMediaObject[socialMedia] = []
+
+            table.update({"socials": socialMediaObject})
+
+    table.update(obj, doc_ids=[1])
+
+# formatter function that sends the correct social media post
+
+
+async def formatter(user, prevTime, socialMedia, channel):
+    platform = socialMedia.capitalize()
+    posts = globals()[f"getLatest{platform}Posts"](user, prevTime)
+    for p in posts:
+        embed = discord.Embed(
+            description=p["post_text"], color=socialsData[socialMedia]["color"], timestamp=datetime.utcfromtimestamp(p["post_timestamp"]).replace(tzinfo=timezone.utc))
+        embed.set_author(
+            name=user, url=p["profile_URL"], icon_url=p["profile_pic_URL"])
+        embed.set_footer(
+            text=platform, icon_url=socialsData[socialMedia]["icon"])
+
+        if p.get("post_media_URL"):
+            embed.set_image(url=p["post_media_URL"])
+        try:
+            await channel.send(
+                content=f"**New post from {user} on {platform}**\n{p['post_URL']}\n{'Click to view video' if p.get('post_isVideo') else ''}", embed=embed)
+        except Exception as e:
+            print(repr(e))
+
+# discord bot commands
+bot = commands.Bot(command_prefix='s!')
+
+
+async def hasPerms(ctx):
+    hasPerms = ctx.author.permissions_in(ctx.channel).manage_channels
+    if not hasPerms:
+        await ctx.send("You do not have permission to use this command.")
+    return hasPerms
+
+
+async def addReaction(ctx): await ctx.message.add_reaction("✅")
+
+
+def to_lower(arg): return arg.lower()
+
+
+@bot.event
+async def on_ready():
+    # printing out message so it looks cool
+    print(f'{bot.user.name} has connected to Discord!')
+
+
+@tasks.loop(minutes=10.0)  # repeat every 10 minutes
+async def mainLoop():
+    await bot.wait_until_ready()
+    threadsFunctions = []
+    for serverID in db.tables():
+        if(doc(serverID).get("prevTime") == None):
+            updateDoc(serverID, {"prevTime": int(time.time())})
+        channel = bot.get_channel(doc(serverID).get("channelID"))
+        if(channel == None):
+            continue
+        prevTime = doc(serverID).get("prevTime")
+        socials = doc(serverID).get("socials")
+        for socialMedia in socialsData.keys():
+            for user in socials[socialMedia]:
+                params = [user, prevTime, socialMedia, channel]
+                threadsFunctions.append(params)
+        updateDoc(serverID, {"prevTime": int(time.time())})
+    for params in threadsFunctions:
+        await formatter(params[0], params[1], params[2], params[3])
+
+
+# ping will respond pong to ensure that the bot is alive
+@ bot.command()
+async def ping(ctx):
+    await ctx.send('Pong')
+    await addReaction(ctx)
+
+
+@ bot.command()
+async def setChannel(ctx, channel: discord.TextChannel = None):
+    if not await hasPerms(ctx):
+        return
+
+    if channel:
+        updateDoc(ctx.guild.id, {"channelID": channel.id})
+    else:
+        updateDoc(ctx.guild.id, {"channelID": ctx.channel.id})
+
+    if(doc(ctx.guild.id).get("prevTime") == None):
+        updateDoc(ctx.guild.id, {"prevTime": int(time.time())})
+
+    await addReaction(ctx)
+
+
+@ setChannel.error
+async def setChannel_error(ctx, error):
+    if isinstance(error, commands.ArgumentParsingError):
+        await ctx.send("Incorrect usage of command: `s!setChannel #{text-channel}`")
+
+
+@ bot.command()
+async def add(ctx, socialMedia: to_lower, user: str):
+    if not await hasPerms(ctx):
+        return
+
+    # checking the first argument (platform management)
+    if socialMedia not in socialsData.keys():
+        await ctx.send(f"Invalid social media site entered. Available social media platforms are {', '.join(socialsData.keys())}.")
+        return
+
+    platform = socialMedia.capitalize()
+    # checks if user account doesn't exist
+    if not globals()[f"check{platform}User"](user):
+        await ctx.send(f"`User {user}` does not exist on {platform}.")
+        return
+
+    # checks if user exists in database already
+    if user in doc(ctx.guild.id)["socials"][socialMedia]:
+        await ctx.send(f"Updates from `{user}` already exist.")
+        return
+
+    socialsObj = doc(ctx.guild.id)["socials"]
+    socialsObj[socialMedia] = [*socialsObj[socialMedia], user]
+
+    updateDoc(ctx.guild.id, {"socials": socialsObj})
+
+    await addReaction(ctx)
+
+
+@ add.error
+async def add_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument) or isinstance(error, commands.ArgumentParsingError):
+        await ctx.send("Incorrect usage of command: `s!add {social-media-site} {username}`")
+
+
+@ bot.command()
+async def remove(ctx, socialMedia: to_lower, user: str):
+    if not await hasPerms(ctx):
+        return
+
+    # checking the first argument (platform management)
+    if socialMedia != "twitter" and socialMedia != "instagram":
+        await ctx.send("Invalid social media site entered. Available social media platforms are `twitter` and `instagram`.")
+        return
+
+    # checks if user exists in database
+    if not user in doc(ctx.guild.id)["socials"][socialMedia]:
+        await ctx.send(f"Updates from `{user}` on {socialMedia} don't exist.")
+        return
+
+    users = doc(ctx.guild.id)["socials"][socialMedia]
+    users.pop(users.index(user))
+
+    socialsObj = doc(ctx.guild.id)["socials"]
+    socialsObj[socialMedia] = users
+
+    updateDoc(ctx.guild.id, {"socials": socialsObj})
+
+    await addReaction(ctx)
+
+
+@ remove.error
+async def remove_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument) or isinstance(error, commands.ArgumentParsingError):
+        await ctx.send("Incorrect usage of command: `s!remove {social-media-site} {username}`")
+
+
+@ bot.command()
+async def list(ctx):
+    if not await hasPerms(ctx):
+        return
+
+    for socialMedia in socialsData:
+        embed = discord.Embed(title="Accounts", description='\n'.join(doc(ctx.guild.id)[
+            "socials"][socialMedia]), color=socialsData[socialMedia]["color"])
+        embed.set_footer(text=socialMedia.capitalize(),
+                         icon_url=socialsData[socialMedia]["icon"])
+        await ctx.send(embed=embed)
+
+    await addReaction(ctx)
+
+mainLoop.start()
+
+if __name__ == '__main__':
+    bot.run(os.getenv('DISCORD_TOKEN'))
